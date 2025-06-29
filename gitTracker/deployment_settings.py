307f8124cda79b76a3bf0706f0b417e0d6355820
debarpanRoy2006 <debarpanroy984@gmail.com>
2025-06-29 21:52:28 +0530

    Your commit message here

--- gitTracker/deployment_settings.py
+++ gitTracker/deployment_settings.py
@@ -40,4 +40,6 @@ DATABASES = {
     )
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
